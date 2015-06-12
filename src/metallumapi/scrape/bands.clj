e126ec7c00912922b5bf293876d49648dd17db24
Cooper Bell <john.cooper.bell@gmail.com>
2015-06-12 16:56:39 -0500

    Scraped band stats and raw band discography info

--- src/metallumapi/scrape/bands.clj
+++ src/metallumapi/scrape/bands.clj
@@ -2,22 +2,67 @@
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as str]))
 
-(def ^:dynamic *base-url* "http://www.metal-archives.com/bands/")
+(def ^:dynamic *base-url* "http://metal-archives.com/")
 
-;;; Selectors
+(def ^:dynamic *band-name* "Deicide")
+(def ^:dynamic *band-id* 148)
+(def ^:dynamic *band-url* (str *base-url* "bands/" *band-name*))
+(def ^:dynamic *discography-url* (str *base-url*
+                            "band/discography/id/"
+                            *band-id* "/tab/"))
+
 (def ^:dynamic *band-content* [:div#band_content])
 
-(def ^:dynamic *band-sidebar* [:div#band_sidebar])
+(def ^:dynamic *band-stats* #{[:div#band_stats :dt]
+                              [:div#band_stats :dd]})
 
-(def ^:dynamic *band-stats* [html/root :> :.band_stats])
+(def ^:dynamic *discography* [:table.discog])
 
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn band-info [band-name]
-  (html/select (fetch-url (str *base-url* band-name))
-               *band-content*))
+(defn drop-colon [strg]
+  (apply str (drop-last strg)))
 
-(defn extract-band-info [node]
-  ())
+(defn pretty-key [ug-str]
+  (-> ug-str drop-colon
+      str/lower-case
+      (str/replace #" " "-")
+      keyword))
+
+(defn split-comma [strg]
+  (map str/trim (str/split strg #",")))
+
+(defn strip-whitespace [strg]
+  (apply str
+         (remove #(#{\tab \space \newline} %) strg)))
+
+
+(defn band-content []
+  (html/select (fetch-url *band-url*) *band-content*))
+
+(defn band-stats [content]
+  (let [stats (reduce-kv (fn [m k v]
+                           (assoc m (pretty-key k) v))
+                         {}
+                         (apply hash-map
+                                (map html/text
+                                     (html/select content *band-stats*))))]
+    (assoc stats
+           :lyrical-themes (-> (:lyrical-themes stats)
+                               str/trim
+                               split-comma)
+           :years-active (-> (:years-active stats)
+                             strip-whitespace
+                             split-comma))))
+
+(defn band-discography []
+  (letfn [(disco [d]
+            (html/select (fetch-url (str *discography-url* d))
+                         *discography*))]
+    {:complete (fn [] (disco "all"))
+     :main (fn [] (disco "main"))
+     :live (fn [] (disco "lives"))
+     :demos (fn [] (disco "demos"))
+     :misc (fn [] (disco "misc"))}))
