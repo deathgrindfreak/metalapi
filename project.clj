e8521a6ccfa49de89f7b0cfb13236e3c12935307
U-MCLANE\LCLADMJCBELL <LCLADMJCBELL@MHO-PF01SZSS.mclane.mclaneco.com>
2015-07-01 14:21:36 -0500

    Added clojurescript/sass/html

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "http://metalapi.com/"
   :license {:name "BSD 3 Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2069"]
                  [enlive "1.1.5"]
-                 [ring "1.4.0-RC1"]])
+                 [ring "1.4.0-RC1"]]
+  :plugins [[lein-cljsbuild "1.0.0"]]
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/main.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]})
